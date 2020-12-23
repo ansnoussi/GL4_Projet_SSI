6ba387a972031cfd1b7bea70fdd34ebfcfdc9b24
ansnoussi <snoussi.anis@insat.u-carthage.tn>
2020-12-23 21:28:27 +0100

    cracking being done, maybe.

--- src/forms/craquage.py
+++ src/forms/craquage.py
@@ -1,14 +1,34 @@
 import npyscreen
+from src.helpers.hachage import HachageHelper
 
-
-class CraquageForm(npyscreen.FormWithMenus):
+class CraquageForm(npyscreen.FormWithMenus, npyscreen.ActionFormMinimal):
     def create(self):
-        self.add(npyscreen.FixedText,value= "CRAQUAGE" )
+
+
+        self.add(npyscreen.FixedText,value= "OPTIONS:" )
         self.how_exited_handers[npyscreen.wgwidget.EXITED_ESCAPE]  = self.exit_application    
         
+        self.Options = npyscreen.OptionList()
+        # just for convenience so we don't have to keep writing Options.options
+        options = self.Options.options
+        
+        options.append(npyscreen.OptionMultiFreeText('Hash', value=''))
+        options.append(npyscreen.OptionSingleChoice('Fonction de hachage', choices=HachageHelper.getAvailable()))
+
+        self.wordListFile = self.add(npyscreen.TitleFilenameCombo,name="Choisir votre dictionnaire des mots", label=True)
+
+
+        self.add(npyscreen.OptionListDisplay, name="Option List", 
+                values = options, 
+                scroll_exit=True,
+                max_height=6)
+
+        self.output = self.add(npyscreen.BoxTitle, name="Output:", max_height=7)
+        self.output.values = []
+
+
         # The menus are created here.
         self.mMain = self.add_menu(name="Menu")
-
         self.mMain.addItemsFromList([
             ("Retour", self.retour),
         ])  
@@ -18,3 +38,8 @@ class CraquageForm(npyscreen.FormWithMenus):
 
     def retour(self, *args, **keywords):
         self.parentApp.change_form("MAIN")
+
+    def afterEditing(self):
+        pass
+    def on_ok(self):
+        self.output.values = [HachageHelper.hash(self.Options.get("Fonction de hachage").value[0], self.Options.get("Hash").value, self.wordListFile.value)]
--- src/helpers/hachage.py
+++ src/helpers/hachage.py
@@ -13,3 +13,14 @@ class HachageHelper:
         b = msg.encode('utf-8')
         h.update(b)
         return h.hexdigest()
+
+    @staticmethod
+    def crackHash(algo, hash, wordlist):
+        h = hashlib.new(algo)
+        with open(wordlist, "r") as infile:
+            for line in infile:
+                line = line.strip().encode('utf-8')
+                h.update(line)
+                lineHash = h.hexdigest()
+                if str(lineHash) == str(hash.lower()):
+                    print("Word is: %s" % line)
