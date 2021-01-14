3d593dd676be13bd1fc92d3a1eed15e9a64c7a2c
ansnoussi <snoussi.anis@insat.u-carthage.tn>
2021-01-14 13:26:09 +0100

    dechiff in place

--- src/forms/dechiffsym.py
+++ src/forms/dechiffsym.py
@@ -1,14 +1,35 @@
 import npyscreen
+from src.helpers.ChiffSym import ChiffSymHelper
 
-
-class DechiffSymForm(npyscreen.FormWithMenus):
+class DechiffSymForm(npyscreen.FormWithMenus, npyscreen.ActionFormMinimal):
     def create(self):
-        self.add(npyscreen.FixedText,value= "DECHIFFREMENT SYMETRIQUE" )
+
+
+        self.add(npyscreen.FixedText,value= "OPTIONS:" )
         self.how_exited_handers[npyscreen.wgwidget.EXITED_ESCAPE]  = self.exit_application    
         
+        self.Options = npyscreen.OptionList()
+        # just for convenience so we don't have to keep writing Options.options
+        options = self.Options.options
+        
+        options.append(npyscreen.OptionMultiFreeText('Votre Message (chiffree)', value=''))
+        options.append(npyscreen.OptionSingleChoice('Type de chiffrement', choices=ChiffSymHelper.getAvailable()))
+        # options.append(npyscreen.TitlePassword('Votre Mot de passe', value=''))        
+
+
+        self.add(npyscreen.OptionListDisplay, name="Option List", 
+                values = options, 
+                scroll_exit=True,
+                max_height=2)
+        self.pwd = self.add(npyscreen.TitlePassword, name = "Password:")
+        
+
+        self.output = self.add(npyscreen.BoxTitle, name="Output:", max_height=4)
+        self.output.values = []
+
+
         # The menus are created here.
         self.mMain = self.add_menu(name="Menu")
-
         self.mMain.addItemsFromList([
             ("Retour", self.retour),
         ])  
@@ -18,3 +39,8 @@ class DechiffSymForm(npyscreen.FormWithMenus):
 
     def retour(self, *args, **keywords):
         self.parentApp.change_form("MAIN")
+
+    def afterEditing(self):
+        pass
+    def on_ok(self):
+        self.output.values = [ChiffSymHelper.decrypt(self.Options.get("Type de chiffrement").value[0], self.Options.get("Votre Message (chiffree)").value, self.pwd.value)]
