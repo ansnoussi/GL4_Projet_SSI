3fcecb0740b7ea2252cbfa3869ae2f93b369a884
ansnoussi <snoussi.anis@insat.u-carthage.tn>
2021-01-15 18:29:58 +0100

    slammed everything together, but it does not work

--- src/helpers/ChiffAsym.py
+++ src/helpers/ChiffAsym.py
@@ -1,8 +1,10 @@
-from Cryptodome.PublicKey import RSA, ElGamal
+from Cryptodome.PublicKey import RSA
+from Crypto.PublicKey import ElGamal
 from Cryptodome.Cipher import PKCS1_OAEP
 from Cryptodome.Signature import pkcs1_15
 from Cryptodome.Hash import SHA256, SHA
-from Cryptodome.Random import get_random_bytes
+from Cryptodome.Random import get_random_bytes, random
+from Cryptodome.Util.number import GCD
 from os import path
 import calendar
 import time
@@ -173,8 +175,6 @@ class ChiffAsymHelper:
             return "The signature is not valid."
 
 
-
-
     #ElGamal
     @staticmethod
     def enc_elgamal(string_to_encrypt,key,pwd):
@@ -202,18 +202,137 @@ class ChiffAsymHelper:
             file_out.write(encrypted_key.encode())
             file_out.close()
 
-        return "done"
+
+        # we choose a k
+        while 1:
+            k = random.StrongRandom().randint(1,int(key.p)-1)
+            if GCD(k,int(key.p)-1)==1: break
+        
+        # we encrypt our data
+        # returns a tuple : u v
+        encrypted_msg = key.encrypt(string_to_encrypt.encode(), k)
+
+        # we get msg ready for export
+        out_msg = str(encrypted_msg[0]) + "\n" + str(encrypted_msg[1])
+
+        # now we save to a file
+        cur_timestamp = calendar.timegm(time.gmtime())
+        file_out = open(str(cur_timestamp) + ".encrypted", "wb")
+        file_out.write(base64.b64encode(out_msg.encode()))
+        file_out.close()
+
+        return "saved to file : " + str(cur_timestamp) + ".encrypted"
 
 
 
     @staticmethod
-    def dec_elgamal(string_to_decrypt,key,pwd):
-        return "1"
+    def dec_elgamal(file_to_decrypt,key,pwd):
+        #first we get the key
+        b64encoded_encrypted_key = open(key, "rb").read()
+        _key = ChiffSymHelper.decrypt("AES" , b64encoded_encrypted_key.decode() , pwd)
+        
+        clean_key_comps = []
+        key_comps = _key.split("\n")
+        for c in key_comps:
+            clean_key_comps.append(int(c.split('=')[1].strip()))
+
+        key = ElGamal.construct(tuple(clean_key_comps))
+
+        #now we get the encrypted msg
+        encoded_msg = open(file_to_decrypt, "rb").read()
+        msg = base64.b64decode(encoded_msg)
+        uv_strings = msg.decode().split("\n")
+
+        uv= []
+        for c in uv_strings:
+            uv.append(c.encode())
+
+        # and now we decrypt it
+
+        d = key.decrypt(tuple(uv))
+
+        return d
+
 
     @staticmethod
-    def sign_elgamal(string_to_decrypt,key,pwd):
-        return "1"
+    def sign_elgamal(string_to_sign,key,pwd):
+        #first we check if key exists:
+        key_name = "elgamal_" + key + ".key"
+        if path.exists(key_name):
+            #we import the key
+            b64encoded_encrypted_key = open(key_name, "rb").read()
+            _key = ChiffSymHelper.decrypt("AES" , b64encoded_encrypted_key.decode() , pwd)
+            
+            clean_key_comps = []
+            key_comps = _key.split("\n")
+            for c in key_comps:
+                clean_key_comps.append(int(c.split('=')[1].strip()))
+
+            key = ElGamal.construct(tuple(clean_key_comps))
+        else:
+            # we create a new key
+            # we could use 2048 but it takes a LONG time
+            key = ElGamal.generate(256, get_random_bytes)
+            out = "\n".join(["{} = {}".format(comp, getattr(key, comp)) for comp in comps])
+            encrypted_key = ChiffSymHelper.encrypt("AES" , out , pwd)
+            #then we save the key
+            file_out = open(key_name, "wb")
+            file_out.write(encrypted_key.encode())
+            file_out.close()
+
+        # we hash the msg to sign
+        h = SHA256.new(string_to_sign.encode())
+
+        # we generate k
+        while 1:
+            k = random.StrongRandom().randint(1,int(key.p)-1)
+            if GCD(k,int(key.p)-1)==1: break
+
+        # and now we sign the hash
+        signature = key.sign(h,k)
+
+        # now we save to a file
+        cur_timestamp = calendar.timegm(time.gmtime())
+        # we save the signature
+        file_out = open(str(cur_timestamp) + ".signed", "wb")
+        file_out.write(base64.b64encode(signature))
+        file_out.close()
+        # we save the original msg
+        hash_out = open(str(str(cur_timestamp) + ".msg"), "wb")
+        hash_out.write(base64.b64encode(string_to_sign.encode()))
+        hash_out.close()
+
+        return "saved signature to file : " + str(cur_timestamp) + ".signed " +  "and the message to file : " + str(cur_timestamp) + ".msg\n" 
     
     @staticmethod
-    def verif_sign_elgamal(string_to_decrypt,key,pwd):
-        return "1"
+    def verif_sign_elgamal(string_to_verif_sig,key,pwd):
+        #first we get the key
+        b64encoded_encrypted_key = open(key, "rb").read()
+        _key = ChiffSymHelper.decrypt("AES" , b64encoded_encrypted_key.decode() , pwd)
+        
+        clean_key_comps = []
+        key_comps = _key.split("\n")
+        for c in key_comps:
+            clean_key_comps.append(int(c.split('=')[1].strip()))
+
+        key = ElGamal.construct(tuple(clean_key_comps))
+
+        # we get the signature
+        encoded_sig = open(string_to_verif_sig, "rb").read()
+        sig = base64.b64decode(encoded_sig)
+
+        # we get the msg
+        hash_file_name = string_to_verif_sig.split(".")[0] + ".msg"
+        hash_file = open(hash_file_name, "rb").read()
+        decode_hash = base64.b64decode(hash_file)
+
+        # we create the hash
+        h = SHA256.new(decode_hash)
+
+
+        #then we verify the sig
+        if key.verify(h,sig):
+            return  "The signature is valid."
+        else:
+            return "The signature is not valid."
+            
